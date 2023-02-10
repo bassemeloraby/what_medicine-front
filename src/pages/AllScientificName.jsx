dbb0a49da2b6c95aa3ad30f123cc91c6f417cc59
bassem eloraby <BASEM12683@GMAIL.COM>
2023-02-10 09:51:49 +0300

    under construction - تحت الانشاء

--- src/pages/AllScientificName.jsx
+++ src/pages/AllScientificName.jsx
@@ -1,3 +1,4 @@
+import { Fragment } from 'react';
 import { FixedSizeList as List } from 'react-window';
 import { useGlobalContext } from '../context';
 
@@ -5,20 +6,29 @@ function AllScientificName() {
   const { drugs } = useGlobalContext();
   console.log(drugs);
   return (
-    <List
-      height={600}
-      itemCount={drugs.length}
-      itemData={drugs}
-      itemSize={35}
-      width={600}
-    >
-      {({ index, style, data }) => (
-        
-        <div style={style} className="card">
-          {data[index].TradeName}
-        </div>
-      )}
-    </List>
+    <Fragment>
+      <div style={{ backgroundColor: 'yellow' }}>
+        <h3 style={{ textAlign: 'center' }}>
+          under construction - تحت الانشاء{' '}
+        </h3>
+      </div>
+
+      <div className="container">
+        <List
+          height={600}
+          itemCount={drugs.length}
+          itemData={drugs}
+          itemSize={35}
+          width={600}
+        >
+          {({ index, style, data }) => (
+            <div style={style} className="card">
+              {data[index].TradeName}
+            </div>
+          )}
+        </List>
+      </div>
+    </Fragment>
   );
 }
 
