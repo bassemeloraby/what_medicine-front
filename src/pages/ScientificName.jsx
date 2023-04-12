8e1dc758347a10bcc98595b66eab32a0978a3eb3
bassem eloraby <BASEM12683@GMAIL.COM>
2023-04-12 17:08:48 +0300

    card update

--- src/pages/ScientificName.jsx
+++ src/pages/ScientificName.jsx
@@ -1,13 +1,40 @@
-import React, {  Fragment } from 'react';
-import { useGlobalContext } from '../context';
+import React, { Fragment, useState, useEffect } from 'react';
 import { useParams, Link } from 'react-router-dom';
+import axios from 'axios';
+import Loading from '../components/Loading';
+
+const url = 'https://sore-lime-goat-tam.cyclic.app/api/drugs';
+
 function ScientificName() {
-  const { drugs } = useGlobalContext();
+  const [drugs, setDrugs] = useState([]);
+  const [loading, setLoading] = useState(false);
   const { ScientificName } = useParams();
+
+  const fetchDrugs = async () => {
+    setLoading(true);
+    try {
+      const res = await axios.get(`${url}`);
+      setLoading(false);
+      setDrugs(res.data);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchDrugs();
+  }, []);
+
+  if (loading) {
+    return <Loading />;
+  }
+
   return (
     <Fragment>
-      <div className="drugs"><h2>{ScientificName}</h2></div>
-      <div className='underline'></div>
+      <div className="drugs">
+        <h2>{ScientificName}</h2>
+      </div>
+      <div className="underline"></div>
       <Link to="/medicine" className="btn btn-primary button-navigate">
         back to medicine list
       </Link>
