ccc65c5dd4725ea979e9adb76a588d05664eed95
bassem eloraby <BASEM12683@GMAIL.COM>
2023-04-12 21:20:56 +0300

    getOneUser

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,26 @@
 import React from 'react';
+import axios from 'axios';
+
+const URL = "https://sore-lime-goat-tam.cyclic.app/api/users"
 
 const Login = () => {
+  const [user, setUser] = React.useState("");
+
+  //fetch user data from backend
+  React.useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const res = await axios.get(`${URL}`);
+        const data = res.data;
+        setUser(data);
+        console.log(data)
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchUser();
+  }, []);
+
   return (
     <React.Fragment>
       <div className="login-section">
