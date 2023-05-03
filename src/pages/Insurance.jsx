c0d7170fda0441f3c44bba5b14f10bd9fbcf380d
bassem eloraby <BASEM12683@GMAIL.COM>
2023-05-03 15:24:12 +0300

    insurance

--- src/pages/Insurance.jsx
+++ src/pages/Insurance.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
 import YouTube from 'react-youtube';
+import { Link } from 'react-router-dom';
 
 function Insurance() {
   return (
-    <div><section className="home-video">
+    <section className='insurance'>
+    <section className='insurance-companies'>
+    <ul className="links" >
+            <li>
+              <Link to="https://www.sfda.gov.sa/ar/drugs-list" target="_blank">sfda</Link>
+            </li>
+            <li>
+              <Link to="https://onlineservices.bupa.com.sa/" target="_blank">bupa</Link>
+            </li>
+            
+          </ul>
+    </section>
+    <section className="home-video">
     <YouTube videoId="7wX3Pnjecqc" />
-  </section></div>
+  </section></section>
   )
 }
 
