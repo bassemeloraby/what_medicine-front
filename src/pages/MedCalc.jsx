08c5597488c7437b5cbb45cdb3e6e6e98c23d59c
bassem eloraby <95348711+bassemeloraby@users.noreply.github.com>
2023-08-08 07:40:42 +0300

    Update MedCalc.jsx

--- src/pages/MedCalc.jsx
+++ src/pages/MedCalc.jsx
@@ -1,1 +1,18 @@
+import React, { Fragment } from 'react';
 
+
+function MedCalc() {
+  return (
+    <Fragment>
+    
+      <section className="home">
+        <section className="home-header">
+          <h2 className="">Welcome to my med calc</h2>
+        </section>
+        
+      </section>
+    </Fragment>
+  );
+}
+
+export default MedCalc;
