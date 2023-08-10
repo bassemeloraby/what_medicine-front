6c5bcc37c3cf695a2ec75418bc618bb5f0323d76
bassem eloraby <95348711+bassemeloraby@users.noreply.github.com>
2023-08-10 06:53:19 +0300

    Update MedCalc.jsx

--- src/pages/MedCalc.jsx
+++ src/pages/MedCalc.jsx
@@ -9,7 +9,9 @@ function MedCalc() {
         <section className="home-header">
           <h2 className="">Welcome to my med calc</h2>
         </section>
-        
+        <h3>20 drops</h3>
+<p>Yes, there are approximately 20 drops in 1 mL (milliliter). One milliliter is equal to 0.20 teaspoons or 0.06 fluid ounces and can be used to measure small amounts of medical solutions like medications. It's important to remember that the exact measurement may vary depending on how many drops your dropper contains.</p>
+<hr>
       </section>
     </Fragment>
   );
