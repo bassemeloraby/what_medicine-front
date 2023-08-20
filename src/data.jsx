1df71f6cf42dfd12b0acdb1ea6bf9110726ae723
bassem eloraby <95348711+bassemeloraby@users.noreply.github.com>
2023-08-20 15:16:22 +0300

    Update data.jsx

--- src/data.jsx
+++ src/data.jsx
@@ -79,5 +79,5 @@ export const categotyData = [
   { id: 2, name: "Cleanser" },
   { id: 3, name: "Dry and Dehydrated Skin" },
   { id: 4, name: "Atopic" },{ id: 5, name: "HYPERPIGMENTED SKIN" },{ id: 6, name: "Deodorant" },
-];
+
 ];
